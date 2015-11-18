e8a8313f1ff52d023aebe1969e2b5acdf6fd4351
Filibert <arnaud.monteils@isen-lille.com>
2015-11-18 13:03:05 +0100

    creator

--- src/GameStateGenerator.java
+++ src/GameStateGenerator.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 
 public class GameStateGenerator {
+
     public static GameState newBlankGameState(Integer x, Integer y){
 
         List<GameStateRow> gameStateRows = new ArrayList<>(y);
@@ -21,6 +24,19 @@ public class GameStateGenerator {
 
         GameState gs = new GameState(gameStateRows);
         gs.updateNeighbors();
+        return gs;
+    }
+    public static GameState StartNewGame(Integer per_cent,Integer x, Integer y){
+        GameState gs = newBlankGameState(x,y);
+        Random rand = new Random();
+        for(int i = 0; i<x; i++ ){
+            for(int j = 0; j < y; j++){
+                int randomNum = rand.nextInt((100 - 0) + 1) + 0;
+                if(randomNum <= per_cent ){
+                    gs.getXYCell(i,j).setMined(true);
+                }
+            }
+        }
         return gs;
     }
 }
