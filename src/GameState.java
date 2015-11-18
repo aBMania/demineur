105cd7b944fcb9fb1e4ab18810a214a8d34ffdda
aBMania <sylvain.bossut@isen-lille.fr>
2015-11-18 10:14:23 +0100

    GameState update

--- src/GameState.java
+++ src/GameState.java
@@ -1,3 +1,41 @@
+import java.util.*;
+
 public class GameState {
-    
+    private List<GameStateRow> gameStateRows;
+    private Integer sizeX;
+    private Integer sizeY;
+
+    public GameState(List<GameStateRow> gameStateRows) {
+        this.gameStateRows = gameStateRows;
+
+        this.setSizeY(gameStateRows.size());
+        this.setSizeX(gameStateRows.get(0).getCellRow().size());
+    }
+
+    public GameState(Integer sizeX, Integer sizeY) {
+        this.setSizeX(sizeX);
+        this.setSizeY(sizeY);
+    }
+
+    public Integer getSizeX() {
+        return sizeX;
+    }
+
+    public void setSizeX(Integer sizeX) {
+        if(sizeX <= 0)
+            throw new RuntimeException("GameState X size cannot be null or negative");
+        this.sizeX = sizeX;
+    }
+
+    public Integer getSizeY() {
+        return sizeY;
+    }
+
+    public void setSizeY(Integer sizeY) {
+        if(sizeY <= 0)
+            throw new RuntimeException("GameState X size cannot be null or negative");
+
+        this.sizeY = sizeY;
+    }
+
 }
