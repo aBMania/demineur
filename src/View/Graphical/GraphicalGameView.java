30b3786d2d8569042681c08adf3be0b055a2b861
aBMania <sylvain.bossut@isen-lille.fr>
2015-11-25 10:40:17 +0100

    a bit better

--- src/View/Graphical/GraphicalGameView.java
+++ src/View/Graphical/GraphicalGameView.java
@@ -5,31 +5,31 @@ import Model.GameState.GameState;
 import View.GameView;
 
 import javax.swing.*;
-import java.awt.*;
 import java.util.Observable;
 
 public class GraphicalGameView extends GameView {
 
     private JFrame frame;
+    private GraphicalGridView grid;
     private final static String FRAME_TITLE = "Demineur";
 
 
     public GraphicalGameView(GameState gameState) {
         super(gameState);
 
-        frame = new JFrame(FRAME_TITLE);
+        JFrame frame = new JFrame(FRAME_TITLE);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(gameState.getSizeY(), gameState.getSizeX()));
 
-        for(int x = 0; x < getGameState().getSizeX();x++){
-            for(int y = 0; y < getGameState().getSizeY(); y++){
-                GameCell cell = getGameState().getXYCell(x, y);
-                frame.add(new GraphicalCellView(cell));
-            }
-        }
+        grid = new GraphicalGridView(gameState);
 
+        frame.add(grid);
         frame.pack();
         frame.setVisible(true);
+
+    }
+
+    private JPanel generateButtonPanel() {
+        return null;
     }
 
     @Override
---
+++ src/View/Graphical/GraphicalGridView.java
@@ -0,0 +1,34 @@
+package View.Graphical;
+
+import Model.GameCell.GameCell;
+import Model.GameState.GameState;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphicalGridView extends JPanel {
+
+    private GameState gameState;
+    private List<GraphicalCellView> cellsViews;
+
+    public GraphicalGridView(GameState gameState) {
+        this.gameState = gameState;
+        this.cellsViews = new ArrayList<>();
+
+        setLayout(new GridLayout(gameState.getSizeY(), gameState.getSizeX()));
+
+        for(int x = 0; x < gameState.getSizeX();x++){
+            for(int y = 0; y < gameState.getSizeY(); y++){
+
+                GameCell cell = gameState.getXYCell(x, y);
+                GraphicalCellView cellView = new GraphicalCellView(cell);
+
+                cellsViews.add(cellView);
+                add(cellView);
+            }
+        }
+    }
+
+}
