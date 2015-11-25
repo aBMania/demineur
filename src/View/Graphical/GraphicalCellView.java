69ee09e924dfe6a8159295b2d555effde0bc0f69
Filibert <arnaud.monteils@isen-lille.com>
2015-11-25 09:47:55 +0100

    button

--- src/View/Graphical/GraphicalCellView.java
+++ src/View/Graphical/GraphicalCellView.java
@@ -1,7 +1,39 @@
 package View.Graphical;
 
+import Model.GameCell.GameCell;
+
 import javax.swing.*;
 
 public class GraphicalCellView extends JButton{
+    private GameCell cell;
+    private boolean debug = false;
 
+    public GraphicalCellView(GameCell cell) {
+
+        this.cell = cell;
+
+    }
+    private void setView(){
+        switch (this.cell.getState()){
+            case VISIBLE:
+                if(this.cell.isMined()){
+                }
+                else{
+                    if(this.cell.getNumberBombsNear() == 0){
+                    }
+                    else{
+                    }
+                }
+            case FLAG_EXCLAMATIONMARK:
+            case FLAG_QUESTIONMARK:
+        }
+
+        if(this.cell.isHidden()){
+            if(this.debug && this.cell.isMined()){
+
+            }
+        }
+
+        throw new RuntimeException("Cannot find symbol for this cell");
+    }
 }
