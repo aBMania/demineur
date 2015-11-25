8da772df5242b05ceb5ef74b458c929f003dd20f
aBMania <sylvain.bossut@isen-lille.fr>
2015-11-25 09:47:42 +0100

    add dirty graphical view implementation

--- src/View/Graphical/GraphicalGameView.java
+++ src/View/Graphical/GraphicalGameView.java
@@ -1,14 +1,35 @@
 package View.Graphical;
 
+import Model.GameCell.GameCell;
 import Model.GameState.GameState;
 import View.GameView;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.Observable;
 
 public class GraphicalGameView extends GameView {
 
+    private JFrame frame;
+    private final static String FRAME_TITLE = "Demineur";
+
+
     public GraphicalGameView(GameState gameState) {
         super(gameState);
+
+        frame = new JFrame(FRAME_TITLE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(gameState.getSizeY(), gameState.getSizeX()));
+
+        for(int y = 0; y < getGameState().getSizeY(); y++){
+            for(int x = 0; x < getGameState().getSizeX();x++){
+                GameCell cell = getGameState().getXYCell(x, y);
+                //frame.add(new GraphicalCellView(cell));
+            }
+        }
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
     @Override
