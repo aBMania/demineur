24f1dcfa032c6eafc5f47b0a788b20795f4b258f
Filibert <arnaud.monteils@isen-lille.com>
2015-12-01 22:47:37 +0100

    test

--- src/View/Graphical/GameComponents/FooterView.java
+++ src/View/Graphical/GameComponents/FooterView.java
@@ -8,15 +8,22 @@ public class FooterView extends JPanel{
 
     private GameState gameState;
     private JLabel sentence;
+    private JLabel image;
 
     public FooterView(GameState gameState) {
+
         this.gameState = gameState;
         sentence = new JLabel("");
+
+        this.add(image);
         this.add(sentence);
     }
 
     public void refresh(){
+        java.net.URL imgURL = getClass().getResource("images/drap.png");
         sentence.setText("Drapeaux restant : " + (gameState.getRemainingFlag()));
+        if(image == null)
+            image = new JLabel(new ImageIcon(imgURL));
     }
 
     private GameState getGameState() {
