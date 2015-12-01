603d52fe46bacb88fd69c9bec144aaadcc81aeca
Filibert <arnaud.monteils@isen-lille.com>
2015-12-01 20:03:02 +0100

    icon interrogation icon flag

--- src/View/Graphical/GameComponents/CellView.java
+++ src/View/Graphical/GameComponents/CellView.java
@@ -7,15 +7,35 @@ import java.awt.*;
 
 public class CellView extends JButton{
     private GameCell cell;
+    private ImageIcon flag;
+    private ImageIcon interrogationMark;
 
     public CellView(GameCell cell, Dimension d) {
+
+        ImageIcon flagTemp = createImageIcon("images/drap.png","?");
+        ImageIcon interrogationMarkTemp = createImageIcon("images/Interro.gif","?");
+        int h = (int)d.getHeight();
+        int w = (int)d.getWidth();
+        flag = new ImageIcon(flagTemp.getImage().getScaledInstance(h,w,Image.SCALE_DEFAULT));
+        interrogationMark = new ImageIcon(interrogationMarkTemp.getImage().getScaledInstance(h,w,Image.SCALE_DEFAULT));
+
         this.cell = cell;
 
         setPreferredSize(d);
 
         refresh();
     }
-
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected ImageIcon createImageIcon(String path,
+                                        String description) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
     public void refresh(){
         switch (this.cell.getState()){
             case VISIBLE:
@@ -28,10 +48,10 @@ public class CellView extends JButton{
                 }
                 break;
             case FLAG_EXCLAMATIONMARK:
-                this.setText("!");
+                this.setIcon(flag);
                 break;
             case FLAG_QUESTIONMARK:
-                this.setText("?");
+                this.setIcon(interrogationMark);
                 break;
             case HIDDEN:
                 this.setText(null);
