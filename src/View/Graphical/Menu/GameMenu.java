c67182d4f71e22ca167594d5817bfdb451a9384b
aBMania <sylvain.bossut@isen-lille.fr>
2015-12-08 20:56:03 +0100

    add custom parameter controller

--- src/View/Graphical/Menu/GameMenu.java
+++ src/View/Graphical/Menu/GameMenu.java
@@ -1,6 +1,8 @@
 package View.Graphical.Menu;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 public class GameMenu extends JMenuBar {
     private final JMenu gameMenu;
@@ -13,24 +15,36 @@ public class GameMenu extends JMenuBar {
 
     public GameMenu() {
         gameMenu = new JMenu("Game");
+        gameMenu.setMnemonic(KeyEvent.VK_G);
         this.add(gameMenu);
 
         newMenu = new JMenu("New");
+        newMenu.setMnemonic(KeyEvent.VK_N);
         gameMenu.add(newMenu);
 
         beginnerMenuItem = new JMenuItem("Beginner");
+        beginnerMenuItem.setMnemonic(KeyEvent.VK_B);
+
+        beginnerMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK));
         newMenu.add(beginnerMenuItem);
 
         intermediateMenuItem = new JMenuItem("Intermediate");
+        intermediateMenuItem.setMnemonic(KeyEvent.VK_I);
+        intermediateMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I, ActionEvent.CTRL_MASK));
         newMenu.add(intermediateMenuItem);
 
         expertMenuItem = new JMenuItem("Expert");
+        expertMenuItem.setMnemonic(KeyEvent.VK_E);
+        expertMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
         newMenu.add(expertMenuItem);
 
         customMenuItem = new JMenuItem("Custom");
+        customMenuItem.setMnemonic(KeyEvent.VK_C);
+        customMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
         newMenu.add(customMenuItem);
 
         quitMenuItem = new JMenuItem("Quit");
+        quitMenuItem.setMnemonic(KeyEvent.VK_Q);
         gameMenu.add(quitMenuItem);
     }
 
